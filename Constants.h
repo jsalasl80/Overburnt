eb7fec4466765b94de7c2e879cabf42225f92b3f
Evan <153223194+5n4v3@users.noreply.github.com>
2024-05-11 17:17:02 -0600

    Includes and Constants header files created
    The idea is to have all includes needed in a single header file, and constants in another header file.

---
+++ Constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS.H
+#define CONSTANTS.H
+
+//Miscellaneous
+#define NONE 0
+
+//May need to change to milliseconds
+#define PREP_DELAY 2
+#define MIN_PREP_TIME 10
+
+#define EATING_DELAY 3
+#define MIN_EATING_TIME 10
+
+
+#endif
---
+++ Includes.h
@@ -0,0 +1,11 @@
+#ifndef INCLUDES.H
+#define INCLUDES.H
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+
+
+#endif
