c0bb02c2fb14f4eedb05bbdbe305cc3a1821efbf
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2024-05-15 20:00:35 -0600

    Create Customer.cpp

---
+++ Customer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Customer{
+    private:
+        int id;
+        std::string name;
+        std::string status;
+        //Order order;// make order object
+
+}
