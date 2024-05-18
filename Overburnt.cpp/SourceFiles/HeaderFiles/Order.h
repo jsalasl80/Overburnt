1817d801d9c950922bc396a073f8182efb5ec63a
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2024-05-18 02:51:41 -0600

    updating classes

---
+++ Overburnt.cpp/SourceFiles/HeaderFiles/Order.h
@@ -0,0 +1,26 @@
+#ifndef ORDER_H
+#define ORDER_H
+
+#include "Includes.h"
+#include "Constants.h"
+#include "Recipe.h"
+
+class Order {
+private:
+    Recipe recipeId;
+    int waitTime;
+
+public:
+    Order(int recipeId){
+        this->recipeId = recipeId;
+    }
+
+    int getRecipeId();
+    int getWaitTime();
+
+    void calculateWaitTime();
+    bool isReady();
+    void markAsCompleted();
+};
+
+#endif // ORDER_H
