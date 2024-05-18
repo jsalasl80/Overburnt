ff9cefedb0c80319658e6095685036c36ed515fe
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2024-05-17 20:27:47 -0600

    .h implemmentation

---
+++ Customer.h
@@ -0,0 +1,12 @@
+#include "Includes.h"
+#include "Constants.h"
+
+using namespace std;
+class Customer{
+    private:
+        int id;
+        std::string name;
+        std::string status;
+        //Order order;// make order object
+
+}
---
+++ Order.h
@@ -0,0 +1,10 @@
+#include "Includes.h"
+#include "Constants.h"
+
+using namespace std;
+
+class Order{
+    private:
+        int recipeID;
+        int waitTime;
+}
