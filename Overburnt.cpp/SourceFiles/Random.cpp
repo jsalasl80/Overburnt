f9912d7a32f40fe086436ab7072735685107aa8f
Evan <153223194+5n4v3@users.noreply.github.com>
2024-05-19 18:38:02 -0600

    Update Random.cpp

--- Overburnt.cpp/SourceFiles/Random.cpp
+++ Overburnt.cpp/SourceFiles/Random.cpp
@@ -9,11 +9,3 @@ int Random::generateBinaryRandom(int option, int alternative){
     int decider = generateRandomInRange(0, 1);
     return decider == 0? option : alternative;
 }
-
-int main (){
-    Random random;
-    for (int i = 0; i < 30; ++i){
-        printf("In range: %d Binary: %d\n", random.generateRandomInRange(1,6), random.generateBinaryRandom(-1, 1));
-    }
-    return 0;
-}
