4a2c4e4d28607167bab62774334f1ce66082f925
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2024-05-20 21:58:39 -0600

    Table Object

---
+++ Table.h
@@ -0,0 +1,9 @@
+#include "Includes.h"
+#include "Constants.h"
+using namespace std;
+
+class order Table{
+    private:
+        int tableNumber;
+        bool status;//false = empty, true = occupied
+}
