5c0a8c5f85392402368d4a6d1b6efa28935c7ea2
Evan <evan.chencheng1@gmail.com>
2024-05-21 22:38:56 -0600

    Ingredients class cleaned

--- Ingredient.h
+++ Ingredient.h
@@ -1,29 +1,26 @@
-#ifndef INGREDIENT
-#define INGREDIENT
+#ifndef INGREDIENT_H
+#define INGREDIENT_H
+
 #include "Includes.h"
 #include "Constants.h"
 using namespace std;
 
 class Ingredient{
-    private:
-        string name;
-        int totalPortions;
-        int consumedPortions;
-        float unitaryCost;
+private:
+    string name;
+    int totalPortions;
+    int consumedPortions;
+    float unitaryCost;
 
-    public:
-        Ingredient(string _name, int _totalPortions, float _unitaryCost):
-                name(_name),
-                totalPortions(_totalPortions),
-                unitaryCost(_unitaryCost),
-                consumedPortions(NONE){} 
-        string toString();
+public:
+    Ingredient(string&, int, float); 
+    string toString();
 
-        void consumePortions(int);
-        bool checkConsumability(int);
+    void consumePortions(int);
+    bool checkConsumability(int);
 
-        string getIngredientName();
-        int getConsumedPortions();
-        float getUnitaryCost();
+    string getIngredientName();
+    int getConsumedPortions();
+    float getUnitaryCost();
 };
 #endif
