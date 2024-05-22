1c172791c04e732065ab0268dc84ff8d9cc80dfd
Evan <evan.chencheng1@gmail.com>
2024-05-22 08:31:03 -0600

    Recipe and inventory management classes updated
    Lockable map is now inventory i fear

--- InventoryManager.h
+++ InventoryManager.h
@@ -1,42 +1,41 @@
-#ifndef INVENTORYMANAGER
-#define INVENTORYMANAGER
+#ifndef INVENTORYMANAGER_H
+#define INVENTORYMANAGER_H
+
 #include "Includes.h"
 #include "Constants.h"
-
 #include "FileWriter.h"
 #include "Ingredient.h"
 #include "CSVReader.h"
-#include "LockableMap.h"
-
+#include "Inventory.h"
+#include "Accountant.h"
 using namespace std;
 
-class InventoryManager : private CSVReader, private FileWriter{
+class InventoryManager : private CSVReader, private FileWriter {
 private:
-    LockableMap<string, Ingredient*> *inventory;
+    std::condition_variable cv;
+    std::mutex inventoryMutex;
+
+    Inventory *inventory;
     vector<string> ingredientNames; //Optimizable
     void storeIngredient(string, int, float);
+    Accountant *accountant;
 
 public:
-    InventoryManager(LockableMap<string, Ingredient*> *_inventory) : CSVReader(INVENTORY_CSV), FileWriter(REPORT_TXT), inventory(_inventory){}
-
-    ~InventoryManager(){
-        delete inventory;
-    }
+    InventoryManager(Inventory*, Accountant*);
+    ~InventoryManager();
     
     void setUpInventory();
 
-    //for testing purposes
-    bool useIngredient(string, int); 
-    Ingredient* getIngredient(string);
-
-    //Must add lock and unlock slash mutex stuff cuz race conditions
-    bool checkIngredientsAvailability(map<string,int>);
-    void updateInventory(map<string,int>);
+    bool checkIngredientsAvailability(map<string,int>&);
+    void updateInventory(map<string,int>&);
     
     void clearInventory();
 
-    //Must add the winnings part (in development)
     void reportInventoryState();
+    
+    //TESTING PURPOSES
+    bool useIngredient(string, int); 
+    Ingredient* getIngredient(string);
 };
 
 #endif
