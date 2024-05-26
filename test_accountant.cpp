6aac51f54ba27a80fa1575560b2a2262c0cc6011
jsalasl80 <145933539+jsalasl80@users.noreply.github.com>
2024-05-26 16:05:43 -0600

    d

---
+++ test_accountant.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "Accountant.h"
+#include "Recipe.h"
+#include "Ingredient.h"
+
+TEST(AccountantTest, UpdateWinnings) {
+    Accountant accountant;
+    Recipe recipe("Pasta", 10.0, 30, 20, {"Tomato", "Pasta"}, {1, 2});
+    accountant.updateWinnings(&recipe);
+    // Verify that winnings were updated correctly
+}
+
+TEST(AccountantTest, UpdateExpenses) {
+    Accountant accountant;
+    Ingredient ingredient("Tomato", 1, 2.0);
+    accountant.updateExpenses(&ingredient, 3);
+    // Verify that expenses were updated correctly
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
