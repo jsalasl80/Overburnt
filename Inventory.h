08754c8be9c42e3220709cebd488cf27e227ce55
Evan <evan.chencheng1@gmail.com>
2024-05-25 18:17:00 -0600

    Inventory re-added?

---
+++ Inventory.h
@@ -0,0 +1,30 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+#include "Includes.h"
+#include "Constants.h"
+#include "Ingredient.h"
+using namespace std;
+
+class Inventory{
+private:
+    unordered_map<string, Ingredient*> inventory;
+    bool locked;
+
+public:
+    Inventory(): inventory(), locked(UNLOCKED){};
+    ~Inventory(){
+        clearInventory();
+    }
+    
+    void addIngredient(string, Ingredient*);
+    Ingredient* getIngredient(string);
+    void clearInventory();
+    bool getAvailability() const;
+    void lock();
+    void unlock();
+    int getSize();
+
+};
+
+#endif //INVENTORY_H
