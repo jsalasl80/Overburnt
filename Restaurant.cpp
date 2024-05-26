c554c4bce71548fe3482cf57f476922b548d2577
Evan <evan.chencheng1@gmail.com>
2024-05-25 20:59:22 -0600

    Final classes added, bin for tests added

---
+++ Restaurant.cpp
@@ -0,0 +1,109 @@
+#include "Restaurant.h"
+
+Restaurant::Restaurant(){
+    menu = new Menu();
+    recipeReader = new RecipeReader(menu);
+    accountant = new Accountant();
+
+    customersInLine = new CustomersInLine();
+    customerSpawner = new CustomerSpawner(customersInLine);
+
+    ordersToDo = new ResultsQueue<Order*>();
+
+    customersUnsatisfied = new ResultsQueue<int>();
+    
+    kitchen = new Kitchen(accountant, ordersToDo);
+
+    setTables();
+    threadPoolTables = new ThreadPoolTables(tables, customersInLine);
+
+    simulating = true;
+}
+
+Restaurant::~Restaurant(){
+    delete kitchen;
+    delete menu;
+    delete recipeReader;
+    delete accountant;
+    cleanTables();
+}
+
+void Restaurant::startRestaurantSimulation(){
+    std::thread tKitchen (&Kitchen::startOperating, kitchen);
+    recipeReader -> setUpMenu();
+
+    std::thread tSpawner(&CustomerSpawner::spawnClients, customerSpawner);
+    std::thread poolTables(&ThreadPoolTables::run, threadPoolTables);
+    std::thread tUnsatisfaction(&Restaurant::updateUnsatisfactionCount, this);
+    
+    tSpawner.join();
+    tKitchen.join();
+    poolTables.join();
+    tUnsatisfaction.join();
+}
+
+void Restaurant::stopRestaurantSimulation(){
+    customerSpawner -> stopSpawning();
+    kitchen -> stopOperating();
+    threadPoolTables -> stopRunning();
+    printf("A total of %d unsatisfied customers registered", checkUnsatisfactionCount);
+    reportRestaurantCurrentState();
+    simulating = false;
+}
+
+void Restaurant::updateUnsatisfactionCount(){
+    while (simulating){
+        bool empty = customersUnsatisfied -> isEmpty();
+        if (!empty){
+            customersUnsatisfiedCount += customersUnsatisfied -> dequeue();
+
+            checkUnsatisfactionCount();
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(MILLI_TO_SEC_CONV));
+    }
+}
+
+void Restaurant::checkUnsatisfactionCount(){
+    if(customersUnsatisfiedCount > MAX_UNSATISFIED_CUSTOMERS){
+        printf("More than 100 unsatisfied clients registered, closing restaurant for today\n");
+        stopRestaurantSimulation();
+    }
+}
+
+void Restaurant::setTables(){
+    tables = new Table*[TABLES_AMOUNT];
+    Menu *pMenu = menu;
+    InventoryManager *inventoryManager = kitchen -> getInventoryManager();
+    
+    ResultsQueue<int> *pCustomersUnsatisfied = customersUnsatisfied;
+    for (int i = 0; i < TABLES_AMOUNT; ++ i){
+        tables[i] = new Table(i, pMenu, inventoryManager, accountant, ordersToDo, pCustomersUnsatisfied);
+    }
+}
+
+void Restaurant::cleanTables(){
+    for (int i = 0; i < TABLES_AMOUNT; ++ i){
+        delete tables[i];
+    }
+    delete[] tables;
+}
+
+string Restaurant::getTableState(int tableId){
+    return tables[tableId] -> reportCurrentState();
+}
+
+Table** Restaurant::getTables(){
+    return tables;
+}
+
+void Restaurant::reportRestaurantCurrentState(){
+    kitchen -> reportCurrentState();
+}
+
+int Restaurant::getUnsatisfactionCount(){
+    return customersUnsatisfiedCount;
+}
+
+int Restaurant::getCustomersInLineCount(){
+    return customersInLine -> getCustomersInLine();
+}
---
+++ Restaurant.h
@@ -0,0 +1,53 @@
+#ifndef RESTAURANT_H
+#define RESTAURANT_H
+
+#include "Includes.h"
+#include "Constants.h"
+#include "Menu.h"
+#include "Kitchen.h"
+#include "CustomerSpawner.h"
+#include "Table.h"
+
+#include "ThreadPoolTables.h"
+#include "CustomersInLine.h"
+#include "ResultsQueue.h"
+
+class Restaurant{
+private:
+    Kitchen *kitchen;
+    CustomerSpawner *customerSpawner;
+    Menu *menu;
+    RecipeReader *recipeReader;
+    Table **tables;
+    Accountant *accountant;
+
+    ThreadPoolTables *threadPoolTables;
+
+    CustomersInLine *customersInLine;
+    ResultsQueue<Order*> *ordersToDo;
+    ResultsQueue<int> *customersUnsatisfied;
+
+    int customersUnsatisfiedCount;
+    bool simulating;
+
+public:
+    Restaurant();
+    ~Restaurant();
+
+    void startRestaurantSimulation();
+    void stopRestaurantSimulation();
+    void updateUnsatisfactionCount();
+    void checkUnsatisfactionCount();
+
+    void setTables();
+    void cleanTables();
+
+    string getTableState(int);
+    Table** getTables();
+    void reportRestaurantCurrentState();
+    int getUnsatisfactionCount();
+    int getCustomersInLineCount();
+    
+};
+
+#endif
