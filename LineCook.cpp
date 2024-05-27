08e9a848851b6aae3f074a31a10ec76674fcb22f
jsalasl80 <jorge.salaslau@ucr.ac.cr>
2024-05-27 00:58:30 -0600

    Revert "Update LineCook.cpp"
    This reverts commit 66d29d735c94a0c8b6f69ff33f9c69618b8b3040.

--- LineCook.cpp
+++ LineCook.cpp
@@ -1,92 +1,43 @@
-// test_threadpoollinecooks.cpp
-
-#include <gtest/gtest.h>
-#include "ThreadPoolLineCooks.h"
 #include "LineCook.h"
-#include "Order.h"
-#include "ResultsQueue.h"
-#include "Customer.h"
-#include "Recipe.h"
 
-class ThreadPoolLineCooksTest : public ::testing::Test {
-protected:
-    ThreadPoolLineCooks* threadPoolLineCooks;
-    ResultsQueue<Order*>* ordersToDo;
-    LineCook** lineCooks;
-    Customer* customer;
-    Recipe* recipe;
-    Order* order;
+LineCook::LineCook(ResultsQueue<Order*> *_completedOrdersQueue):
+    state(CHILLING),
+    ordersToDeliver(_completedOrdersQueue)
+    {}
 
-    void SetUp() override {
-        ordersToDo = new ResultsQueue<Order*>();
+void LineCook::prepareOrder(Order *order){
+    std::lock_guard<mutex> lg(lineCookMutex);
+    printf("Line cook preparing order\n");
+    this->order = order;
+    cookOrder();
+    order -> markAsCompleted();
+    deliverOrder();
+    this -> setState(CHILLING);
+    printf("Line cook chilling\n");
+}
 
-        customer = new Customer(1, "John Doe");
-        std::vector<std::string> ingredients = {"Tomato", "Pasta"};
-        std::vector<int> amounts = {2, 3};
-        recipe = new Recipe("Pasta", 10.0, 15, 10, ingredients, amounts);
-        order = new Order(1, 1, recipe, customer);
+void LineCook::cookOrder(){
+    int preparationTime = order -> getOrderPrepTime();
+    printf("Preparing %s for %i milliseconds\n", order->getRecipe() -> getRecipeName().c_str(), preparationTime);
+    std::this_thread::sleep_for(std::chrono::milliseconds(preparationTime));
+}
 
-        // Create LineCooks
-        lineCooks = new LineCook*[LINE_COOKS_AMOUNT];
-        for (int i = 0; i < LINE_COOKS_AMOUNT; ++i) {
-            lineCooks[i] = new LineCook();
-        }
+void LineCook::deliverOrder(){
+    ordersToDeliver -> enqueue(order);
+}
 
-        threadPoolLineCooks = new ThreadPoolLineCooks(lineCooks, ordersToDo);
+bool LineCook::getAvailability(){
+    bool availability = COOKING;
+    if (lineCookMutex.try_lock()){
+        availability = state;
+        lineCookMutex.unlock();
     }
-
-    void TearDown() override {
-        delete threadPoolLineCooks;
-        delete ordersToDo;
-        delete order;
-        delete recipe;
-        delete customer;
-
-        for (int i = 0; i < LINE_COOKS_AMOUNT; ++i) {
-            delete lineCooks[i];
-        }
-        delete[] lineCooks;
-    }
-};
-
-TEST_F(ThreadPoolLineCooksTest, GetAvailableLineCook) {
-    LineCook* availableLineCook = threadPoolLineCooks->getAvailableLineCook();
-    ASSERT_NE(availableLineCook, nullptr);
-    EXPECT_EQ(availableLineCook->getState(), COOKING);
+    return availability;
 }
 
-TEST_F(ThreadPoolLineCooksTest, AddLineCookToRotation) {
-    LineCook* lineCook = threadPoolLineCooks->getAvailableLineCook();
-    ordersToDo->enqueue(order);
-    bool added = threadPoolLineCooks->addLineCookToRotation(lineCook);
-    EXPECT_TRUE(added);
+void LineCook::setState(bool _state){
+    state = _state;
 }
-
-TEST_F(ThreadPoolLineCooksTest, RunAndStop) {
-    std::thread poolThread(&ThreadPoolLineCooks::run, threadPoolLineCooks);
-    ordersToDo->enqueue(order);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Wait to ensure threads start
-    threadPoolLineCooks->stopRunning();
-    poolThread.join();
-
-    for (int i = 0; i < LINE_COOKS_AMOUNT; ++i) {
-        if (!lineCooks[i]->isAvailable()) {
-            EXPECT_EQ(lineCooks[i]->getState(), COOKING);
-        }
-    }
-}
-
-TEST_F(ThreadPoolLineCooksTest, StopRunning) {
-    std::thread poolThread(&ThreadPoolLineCooks::run, threadPoolLineCooks);
-    ordersToDo->enqueue(order);
-    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Wait to ensure threads start
-    threadPoolLineCooks->stopRunning();
-    poolThread.join();
-
-    EXPECT_FALSE(threadPoolLineCooks->running);
-}
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+bool LineCook::getState(){
+    return state;
 }
